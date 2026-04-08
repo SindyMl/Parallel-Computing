ac6ebb168e461bdf496755e7d26c07c3eee2ed92
Sindiswa Mulondo <chantymulondo@gmail.com>
2026-04-08 03:32:20 +0200

    Enhance run.sh with output directory and messages
    Added output directory creation and improved output messages.

--- pc_assignment_1_2026/base_codes/src/run.sh
+++ pc_assignment_1_2026/base_codes/src/run.sh
@@ -1,4 +1,8 @@
-echo ------------Fractal is starting---------------
-./fractal |& tee -a terminal.out
-echo -------------Fractal is done------------------
+#!/bin/bash
+mkdir -p output
+echo "------------ Fractal is starting ---------------"
+./fractal |& tee terminal.out
+echo "------------- Fractal is done ------------------"
 echo
+echo "Results saved to terminal.out"
+echo "Images saved to output/"
